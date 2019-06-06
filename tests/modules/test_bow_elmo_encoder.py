07b5f4ab0355daf87daa02dd28cc299950cbbfa9
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-06 12:07:53 +0800

    1)torch.device is passed to engine 2) tests need to be changed because of the change in api interface with bow_elmo_encoder 3) lstm2vecencoder should have initial vectors on gpu if the model is being run on gpu

--- tests/modules/test_bow_elmo_encoder.py
+++ tests/modules/test_bow_elmo_encoder.py
@@ -1,5 +1,6 @@
 import pytest
 from parsect.modules.bow_elmo_encoder import BowElmoEncoder
+from parsect.utils.common import pack_to_length
 
 
 @pytest.fixture
@@ -8,6 +9,12 @@ def setup_bow_elmo_encoder_layer_agg_sum_word_agg_sum():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(layer_aggregation="sum", word_aggregation="sum")
     return bow_elmo_encoder, instances
 
@@ -18,6 +25,12 @@ def setup_bow_elmo_encoder_layer_agg_average_word_agg_sum():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(
         layer_aggregation="average", word_aggregation="sum"
     )
@@ -31,6 +44,12 @@ def setup_bow_elmo_encoder_layer_agg_last_word_agg_sum():
         "I like to go out on long drives in a car".split(),
     ]
 
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(layer_aggregation="last", word_aggregation="sum")
     return bow_elmo_encoder, instances
 
@@ -41,6 +60,13 @@ def setup_bow_elmo_encoder_layer_agg_first_word_agg_sum():
         "I like to eat carrots".split(),
         "I like to go out on long drives in a car".split(),
     ]
+
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(layer_aggregation="first", word_aggregation="sum")
     return bow_elmo_encoder, instances
 
@@ -51,6 +77,13 @@ def setup_bow_elmo_encoder_layer_agg_sum_word_agg_average():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(
         layer_aggregation="sum", word_aggregation="average"
     )
@@ -63,6 +96,13 @@ def setup_bow_elmo_encoder_layer_agg_average_word_agg_average():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(
         layer_aggregation="average", word_aggregation="average"
     )
@@ -75,6 +115,13 @@ def setup_bow_elmo_encoder_layer_agg_last_word_agg_average():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(
         layer_aggregation="last", word_aggregation="average"
     )
@@ -87,6 +134,13 @@ def setup_bow_elmo_encoder_layer_agg_first_word_agg_average():
         "I like to eat carrot".split(),
         "I like to go out on long drives in a car".split(),
     ]
+
+    padded_instances = []
+    for instance in instances:
+        padded_inst = pack_to_length(tokenized_text=instance, max_length=10)
+        padded_instances.append(padded_inst)
+    instances = padded_instances
+
     bow_elmo_encoder = BowElmoEncoder(
         layer_aggregation="first", word_aggregation="average"
     )
