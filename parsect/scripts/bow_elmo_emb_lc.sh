0c947ac7f4e132477b802afdb1be53a28a2b648c
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-06 10:15:27 +0800

    added initial list of experiments for bow elmo linear classifier

--- parsect/scripts/bow_elmo_emb_lc.sh
+++ parsect/scripts/bow_elmo_emb_lc.sh
@@ -3,9 +3,9 @@
 cd ../clients
 
 python bow_elmo_emb_linear_classifier.py \
---exp_name "debug_bow_elmo_emb_lc_10e_1e-4lr" \
+--exp_name "debug_bow_elmo_emb_lc_70e_1e-4lr" \
 --max_length 15 \
---device cpu \
+--device cuda:0 \
 --debug \
 --layer_aggregation last \
 --word_aggregation sum \
@@ -17,37 +17,89 @@ python bow_elmo_emb_linear_classifier.py \
 --save_every 1 \
 --log_train_metrics_every 10
 
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_last_wordagg_sum_10e_1e-3lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation last \
+--word_aggregation sum \
+--debug_dataset_proportion 0.01 \
+--bs 10 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 10 \
+--save_every 1 \
+--log_train_metrics_every 10
 
-#python bow_elmo_emb_linear_classifier.py \
-#--exp_name "bow_elmo_emb_lc_20e_1e-3lr_1" \
-#--device cpu \
-#--debug_dataset_proportion 0.01 \
-#--bs 32 \
-#--emb_dim 1024 \
-#--lr 1e-3 \
-#--epochs 20 \
-#--save_every 1 \
-#--log_train_metrics_every 50
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_last_wordagg_sum_40e_1e-4lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation last \
+--word_aggregation sum \
+--debug_dataset_proportion 0.01 \
+--bs 10 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 10
 
 
-#python bow_elmo_emb_linear_classifier.py \
-#--exp_name "bow_elmo_emb_lc_40e_1e-4lr" \
-#--device cpu \
-#--debug_dataset_proportion 0.01 \
-#--bs 32 \
-#--emb_dim 1024 \
-#--lr 1e-4 \
-#--epochs 40 \
-#--save_every 1 \
-#--log_train_metrics_every 50
-#
-#python bow_elmo_emb_linear_classifier.py \
-#--exp_name "bow_elmo_emb_lc_50e_1e-4lr" \
-#--device cpu \
-#--debug_dataset_proportion 0.01 \
-#--bs 32 \
-#--emb_dim 1024 \
-#--lr 1e-4 \
-#--epochs 50 \
-#--save_every 1 \
-#--log_train_metrics_every 50
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_first_wordagg_sum_20e_1e-4lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation first \
+--word_aggregation sum \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_first_wordagg_sum_40e_1e-4lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation first \
+--word_aggregation sum \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_first_wordagg_average_40e_1e-4lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation first \
+--word_aggregation average \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+python bow_elmo_emb_linear_classifier.py \
+--exp_name "bow_elmo_emb_lc_layeragg_average_wordagg_sum_40e_1e-4lr" \
+--max_length 15 \
+--device cuda:0 \
+--layer_aggregation average \
+--word_aggregation sum \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_dim 1024 \
+--lr 1e-4 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
