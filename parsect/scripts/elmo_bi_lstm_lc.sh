c1a5537a9e0a2fbbd8dae493612ea1e3715cc48e
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-06 15:31:46 +0800

    elmo bi lstm experiments

--- parsect/scripts/elmo_bi_lstm_lc.sh
+++ parsect/scripts/elmo_bi_lstm_lc.sh
@@ -16,6 +16,165 @@ python elmo_bi_lstm_encoder_linear_classifier.py \
 --lr 1e-3 \
 --bidirectional \
 --combine_strategy concat \
---epochs 40 \
+--epochs 25 \
 --save_every 1 \
 --log_train_metrics_every 5
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_1kw_ml10_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 1000 \
+--max_length 10 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 10
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_2kw_ml10_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 2000 \
+--max_length 10 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_3kw_ml10_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 3000 \
+--max_length 10 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_5kw_ml10_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 5000 \
+--max_length 10 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_8kw_ml10_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 8000 \
+--max_length 10 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_5kw_ml15_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 5000 \
+--max_length 15 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_5kw_ml20_300d_512h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 5000 \
+--max_length 20 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 512 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_5kw_ml20_300d_256h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 5000 \
+--max_length 20 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 256 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python elmo_bi_lstm_encoder_linear_classifier.py \
+--exp_name "elmo_bi_lstm_lc_5kw_ml20_300d_128h_1e-3_bidir_concat_20e" \
+--device cuda:0 \
+--max_num_words 5000 \
+--max_length 20 \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type random \
+--emb_dim 300 \
+--hidden_dim 128 \
+--lr 1e-3 \
+--bidirectional \
+--combine_strategy concat \
+--epochs 20 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+
+
