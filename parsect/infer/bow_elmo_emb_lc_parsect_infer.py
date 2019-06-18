fd5100009848cec370f86dbf6397af767b925c68
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-18 13:01:30 +0800

    bow elmo parsect infer dataset instantiation

--- parsect/infer/bow_elmo_emb_lc_parsect_infer.py
+++ parsect/infer/bow_elmo_emb_lc_parsect_infer.py
@@ -4,9 +4,12 @@ import parsect.constants as constants
 from parsect.clients.parsect_inference import ParsectInference
 from parsect.models.bow_elmo_linear_classifier import BowElmoLinearClassifier
 from parsect.modules.bow_elmo_encoder import BowElmoEncoder
+from parsect.datasets.parsect_dataset import ParsectDataset
 
 PATHS = constants.PATHS
 OUTPUT_DIR = PATHS["OUTPUT_DIR"]
+FILES = constants.FILES
+SECT_LABEL_FILE = FILES["SECT_LABEL_FILE"]
 
 
 def get_elmo_emb_lc_infer_parsect(dirname: str):
@@ -14,10 +17,16 @@ def get_elmo_emb_lc_infer_parsect(dirname: str):
     with open(hyperparam_config_filepath, "r") as fp:
         config = json.load(fp)
 
+    MAX_NUM_WORDS = config["MAX_NUM_WORDS"]
+    MAX_LENGTH = config["MAX_LENGTH"]
+    vocab_store_location = config["VOCAB_STORE_LOCATION"]
+    DEBUG = config["DEBUG"]
+    DEBUG_DATASET_PROPORTION = config["DEBUG_DATASET_PROPORTION"]
     EMBEDDING_DIM = config["EMBEDDING_DIMENSION"]
-    encoder = BowElmoEncoder(emb_dim=EMBEDDING_DIM)
-
+    EMBEDDING_TYPE = config.get("EMBEDDING_TYPE", "random")
     NUM_CLASSES = config["NUM_CLASSES"]
+
+    encoder = BowElmoEncoder(emb_dim=EMBEDDING_DIM)
 
     model = BowElmoLinearClassifier(
         encoder=encoder, encoding_dim=EMBEDDING_DIM, num_classes=NUM_CLASSES
@@ -26,15 +35,28 @@ def get_elmo_emb_lc_infer_parsect(dirname: str):
     MODEL_SAVE_DIR = config["MODEL_SAVE_DIR"]
     model_filepath = os.path.join(MODEL_SAVE_DIR, "best_model.pt")
 
+    dataset = ParsectDataset(
+        secthead_label_file=SECT_LABEL_FILE,
+        dataset_type="test",
+        max_num_words=MAX_NUM_WORDS,
+        max_length=MAX_LENGTH,
+        vocab_store_location=vocab_store_location,
+        debug=DEBUG,
+        debug_dataset_proportion=DEBUG_DATASET_PROPORTION,
+        embedding_type=EMBEDDING_TYPE,
+        embedding_dimension=EMBEDDING_DIM,
+    )
+
     parsect_inference = ParsectInference(
         model=model,
         model_filepath=model_filepath,
         hyperparam_config_filepath=hyperparam_config_filepath,
+        dataset=dataset,
     )
 
     return parsect_inference
 
 
 if __name__ == "__main__":
-    experiment_dirname = os.path.join(OUTPUT_DIR, "debug_bow_elmo_emb_lc_50e_1e-4lr")
+    experiment_dirname = os.path.join(OUTPUT_DIR, "debug_bow_elmo_emb_lc_parsect")
     inference_client = get_elmo_emb_lc_infer_parsect(experiment_dirname)
