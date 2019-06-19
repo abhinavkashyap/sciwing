8ad34bff9958bb2f25533bf34fabde812961bdb2
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-19 09:57:51 +0800

    bow random for gensect

--- parsect/scripts/bow_random_emb_lc_gensect.sh
+++ parsect/scripts/bow_random_emb_lc_gensect.sh
@@ -14,3 +14,87 @@ python bow_random_emb_lc_generic_sect.py \
 --epochs 40 \
 --save_every 1 \
 --log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_300w_ml5_200d_40e" \
+--max_num_words 300 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 5
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_300w_ml5_200d_40e" \
+--max_num_words 300 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_400w_ml5_100d_40e" \
+--max_num_words 400 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 100 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_400w_ml5_200d_40e" \
+--max_num_words 400 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_700w_ml5_200d_40e" \
+--max_num_words 700 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_900w_ml5_200d_40e" \
+--max_num_words 900 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
+
+python bow_random_emb_lc_generic_sect.py \
+--exp_name "bow_random_generic_sect_1000w_ml5_200d_40e" \
+--max_num_words 1000 \
+--max_length 5 \
+--debug_dataset_proportion 0.1 \
+--bs 32 \
+--emb_dim 200 \
+--lr 1e-2 \
+--epochs 40 \
+--save_every 1 \
+--log_train_metrics_every 50
