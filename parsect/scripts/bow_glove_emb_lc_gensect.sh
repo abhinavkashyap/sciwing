a1f69ab071ffd7bb12a252d97afe1050d17cb3ec
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-19 10:10:53 +0800

    bow glove experiments for gensect

--- parsect/scripts/bow_glove_emb_lc_gensect.sh
+++ parsect/scripts/bow_glove_emb_lc_gensect.sh
@@ -15,5 +15,133 @@ python bow_glove_emb_lc_generic_sect.py \
 --emb_dim 50 \
 --lr 1e-3 \
 --epochs 40 \
---save_every 1 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_300w_ml5_50d_40e" \
+--max_num_words 300 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_50 \
+--emb_dim 50 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_400w_ml5_50d_40e" \
+--max_num_words 400 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_50 \
+--emb_dim 50 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_600w_ml5_50d_40e" \
+--max_num_words 400 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_50 \
+--emb_dim 50 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_900w_ml5_50d_40e" \
+--max_num_words 900 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_50 \
+--emb_dim 50 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_300w_ml5_100d_40e" \
+--max_num_words 300 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_100 \
+--emb_dim 100 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_600w_ml5_100d_40e" \
+--max_num_words 600 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_100 \
+--emb_dim 100 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_600w_ml5_100d_40e" \
+--max_num_words 600 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_100 \
+--emb_dim 100 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_900w_ml5_100d_40e" \
+--max_num_words 900 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_100 \
+--emb_dim 100 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
+--log_train_metrics_every 50
+
+
+python bow_glove_emb_lc_generic_sect.py \
+--exp_name "bow_glove_genericsect_900w_ml5_200d_40e" \
+--max_num_words 900 \
+--max_length 5 \
+--debug \
+--debug_dataset_proportion 0.01 \
+--bs 32 \
+--emb_type glove_6B_200 \
+--emb_dim 200 \
+--lr 1e-3 \
+--epochs 40 \
+--save_every 5 \
 --log_train_metrics_every 50
