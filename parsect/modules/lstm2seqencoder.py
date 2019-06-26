fbd1c034fee322dab45b3482e039a04a30d2a98b
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-06-26 19:22:42 +0800

    single layer lstm 2 seq encoders and tests added

---
+++ parsect/modules/lstm2seqencoder.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import wasabi
+
+
+class Lstm2SeqEncoder(nn.Module):
+    def __init__(
+        self,
+        emb_dim: int,
+        embedding: torch.nn.Embedding,
+        dropout_value: float = 0.0,
+        hidden_dim: int = 1024,
+        bidirectional: bool = False,
+        combine_strategy: str = "concat",
+        rnn_bias: bool = False,
+        device=torch.device("cpu"),
+    ):
+        """
+        :param emb_dim: type: int
+        The embedding dimension of the embedding used in the first layer of the LSTM
+        :param embedding: type: torch.nn.Embedding
+        :param dropout_value: type: float
+        :param hidden_dim: type: float
+        :param bidirectional: type: bool
+        Is the LSTM bi-directional
+        :param combine_strategy: type: str
+        This can be one of concat, sum, average
+        This decides how different hidden states of different embeddings are combined
+        :param rnn_bias: type: bool
+        should RNN bias be used
+        This is mainly for debugging purposes
+        use sparingly
+        """
+        super(Lstm2SeqEncoder, self).__init__()
+        self.emb_dim = emb_dim
+        self.embedding = embedding
+        self.dropout_value = dropout_value
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+        self.combine_strategy = combine_strategy
+        self.rnn_bias = rnn_bias
+        self.device = device
+        self.num_directions = 2 if self.bidirectional else 1
+        self.num_layers = 1
+        self.allowed_combine_strategies = ["sum", "concat"]
+        self.msg_printer = wasabi.Printer()
+
+        assert (
+            self.combine_strategy in self.allowed_combine_strategies
+        ), self.msg_printer.fail(
+            f"The combine strategies can be one of "
+            f"{self.allowed_combine_strategies}. You passed "
+            f"{self.combine_strategy}"
+        )
+        self.emb_dropout = nn.Dropout(p=self.dropout_value)
+        self.rnn = nn.LSTM(
+            input_size=self.emb_dim,
+            hidden_size=self.hidden_dim,
+            bias=self.rnn_bias,
+            batch_first=True,
+            bidirectional=self.bidirectional,
+        )
+
+    def forward(
+        self,
+        x: torch.LongTensor,
+        c0: torch.FloatTensor = None,
+        h0: torch.FloatTensor = None,
+    ) -> torch.Tensor:
+        batch_size, seq_length = x.size()
+
+        # batch_size * time steps * embedding dimension
+        embedded_tokens = self.embedding(x)
+        embedded_tokens = self.emb_dropout(embedded_tokens)
+
+        if h0 is None or c0 is None:
+            h0, c0 = self.get_initial_hidden(batch_size=batch_size)
+
+        # output = batch_size, sequence_length, hidden_dim * num_directions
+        # h_n = num_layers * num_directions, batch_size, hidden_dimension
+        # c_n = num_layers * num_directions, batch_size, hidden_dimension
+        output, (_, _) = self.rnn(embedded_tokens, (h0, c0))
+
+        if self.bidirectional:
+            output = output.view(batch_size, seq_length, 2, -1)
+            forward_output = output[:, :, 0, :]  # batch_size, seq_length, hidden_dim
+            backward_output = output[:, :, 1, :]  # batch_size, seq_length, hidden_dim
+            print(f"forward output size {forward_output.size()}")
+            if self.combine_strategy == "concat":
+                encoding = torch.cat([forward_output, backward_output], dim=2)
+            elif self.combine_strategy == "sum":
+                encoding = torch.add(forward_output, backward_output)
+        else:
+            encoding = output
+
+        return encoding
+
+    def get_initial_hidden(self, batch_size: int):
+        h0 = torch.zeros(
+            self.num_layers * self.num_directions, batch_size, self.hidden_dim
+        )
+        c0 = torch.zeros(
+            self.num_layers * self.num_directions, batch_size, self.hidden_dim
+        )
+        h0 = h0.to(self.device)
+        c0 = c0.to(self.device)
+        return h0, c0
---
+++ tests/modules/test_lstm2seqencoder.py
@@ -0,0 +1,73 @@
+import pytest
+from parsect.modules.lstm2seqencoder import Lstm2SeqEncoder
+import torch
+import torch.nn as nn
+import numpy as np
+import itertools
+
+lstm2encoder_options = itertools.product([True, False], ["sum", "concat"])
+lstm2encoder_options = list(lstm2encoder_options)
+
+
+@pytest.fixture(params=lstm2encoder_options)
+def setup_lstm2seqencoder(request):
+    EMBEDDING_DIM = 100
+    VOCAB_SIZE = 1000
+    BATCH_SIZE = 2
+    HIDDEN_DIM = 1024
+    NUM_TIME_STEPS = 10
+    BIDIRECTIONAL = request.param[0]
+    COMBINE_STRATEGY = request.param[1]
+    EMBEDDING = nn.Embedding.from_pretrained(torch.zeros([VOCAB_SIZE, EMBEDDING_DIM]))
+    tokens = np.random.randint(0, VOCAB_SIZE - 1, size=(BATCH_SIZE, NUM_TIME_STEPS))
+    tokens = torch.LongTensor(tokens)
+    encoder = Lstm2SeqEncoder(
+        emb_dim=EMBEDDING_DIM,
+        embedding=EMBEDDING,
+        dropout_value=0.0,
+        hidden_dim=HIDDEN_DIM,
+        bidirectional=BIDIRECTIONAL,
+        combine_strategy=COMBINE_STRATEGY,
+        rnn_bias=False,
+    )
+
+    return (
+        encoder,
+        {
+            "EMBEDDING_DIM": EMBEDDING_DIM,
+            "VOCAB_SIZE": VOCAB_SIZE,
+            "BATCH_SIZE": BATCH_SIZE,
+            "HIDDEN_DIM": HIDDEN_DIM,
+            "COMBINE_STRATEGY": COMBINE_STRATEGY,
+            "BIDIRECTIONAL": BIDIRECTIONAL,
+            "tokens": tokens,
+            "EXPECTED_HIDDEN_DIM": 2 * HIDDEN_DIM
+            if COMBINE_STRATEGY == "concat" and BIDIRECTIONAL
+            else HIDDEN_DIM,
+            "TIME_STEPS": NUM_TIME_STEPS,
+        },
+    )
+
+
+class TestLstm2SeqEncoder:
+    def test_hidden_dim(self, setup_lstm2seqencoder):
+        encoder, options = setup_lstm2seqencoder
+        tokens = options["tokens"]
+        batch_size = options["BATCH_SIZE"]
+        num_time_steps = options["TIME_STEPS"]
+        expected_hidden_size = options["EXPECTED_HIDDEN_DIM"]
+        encoding = encoder(tokens)
+        assert encoding.size() == (batch_size, num_time_steps, expected_hidden_size)
+
+    def test_encoder_produces_zero_encoding(self, setup_lstm2seqencoder):
+        encoder, options = setup_lstm2seqencoder
+        tokens = options["tokens"]
+        batch_size = options["BATCH_SIZE"]
+        num_time_steps = options["TIME_STEPS"]
+        expected_hidden_size = options["EXPECTED_HIDDEN_DIM"]
+        encoding = encoder(tokens)
+        assert torch.all(
+            torch.eq(
+                encoding, torch.zeros(batch_size, num_time_steps, expected_hidden_size)
+            )
+        )
