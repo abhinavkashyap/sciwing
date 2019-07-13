1e55973a0c0dcf621459ee39a7d8d31da456fbfa
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-07-13 13:47:18 +0800

    first full experiment added and running on gpu

--- parsect/scripts/lstm_crf_parscit.sh
+++ parsect/scripts/lstm_crf_parscit.sh
@@ -5,25 +5,45 @@ cd ../clients
 EXPERIMENT_PREFIX="lstm_crf_parscit"
 SCRIPT_FILE="lstm_crf_client.py"
 
+#python ${SCRIPT_FILE} \
+#--exp_name ${EXPERIMENT_PREFIX}"_debug" \
+#--device cpu \
+#--max_num_words 1000 \
+#--max_len 10 \
+#--max_char_len 25 \
+#--debug \
+#--debug_dataset_proportion 0.02 \
+#--bs 10 \
+#--emb_type random \
+#--emb_dim 50 \
+#--char_emb_dim 25 \
+#--hidden_dim 1024 \
+#--lr 1e-2 \
+#--bidirectional \
+#--use_char_encoder \
+#--char_encoder_hidden_dim 100 \
+#--combine_strategy concat \
+#--epochs 10 \
+#--save_every 50 \
+#--log_train_metrics_every 5
+
 python ${SCRIPT_FILE} \
---exp_name ${EXPERIMENT_PREFIX}"_debug" \
---device cpu \
---max_num_words 1000 \
---max_len 10 \
---max_char_len 25 \
---debug \
+--exp_name ${EXPERIMENT_PREFIX}_"no_char_enc_10kw_ml20_100d_1024h_1e-2lr_bidir_concat_100e" \
+--device cuda:0 \
+--max_num_words 10000 \
+--max_len 20 \
+--max_char_len 100 \
 --debug_dataset_proportion 0.02 \
 --bs 10 \
 --emb_type random \
---emb_dim 50 \
+--emb_dim 100 \
 --char_emb_dim 25 \
 --hidden_dim 1024 \
 --lr 1e-2 \
 --bidirectional \
---use_char_encoder \
 --char_encoder_hidden_dim 100 \
 --combine_strategy concat \
---epochs 10 \
---save_every 50 \
---log_train_metrics_every 5
+--epochs 100 \
+--save_every 10 \
+--log_train_metrics_every 10
 
