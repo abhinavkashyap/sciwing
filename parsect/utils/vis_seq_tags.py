c68b28a182eb3043cb0b6bde593d774f245412ca
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-07-16 12:17:58 +0800

    1) Visualising tags for Neural Parscit in a better manner. 2) Parsect cli just prints the sentences without any stylistic modification. Stylistic Modifications should be handled by individual inference classes

---
+++ parsect/utils/vis_seq_tags.py
@@ -0,0 +1,163 @@
+from typing import List, Dict, Any
+import itertools
+import colorful
+from parsect.utils.common import pairwise
+
+
+class VisTagging:
+    def __init__(self, colors: List[str] = None, colors_palette: str = None):
+        """
+        colors: List[str]
+        The set of colors used for tagging
+        color_palette: str
+        You can specify a color palette.
+        We recommend using solarized or monokai and appropriate colors
+        For more options, refer to the documentation of the python package `colorful`
+        json_annotation: str
+        You can send a json that has the following format
+        {'text': str,
+        'tags': [{'start':int, 'end':str, 'tag': str}]
+        }
+
+        Parameters
+        ----------
+        colors: List[str]
+        The set of colors that will be used for tagging
+        colors_palette: str
+        The color palette that should be used. We recommend
+        keeping it as None. For more information on color palettes
+        you can refer to the documentation of the python package `colorful`
+
+        """
+        if colors is None:
+            colors = [
+                "yellow",
+                "orange",
+                "red",
+                "magenta",
+                "violet",
+                "blue",
+                "cyan",
+                "green",
+            ]
+        if colors_palette is None:
+            colors_palette = "solarized"
+
+        self.colors = colors
+        self.colors_palette = colors_palette
+        self.colors_iter = itertools.cycle(self.colors)
+        colorful.use_style(self.colors_palette)
+
+    def _get_next_color(self):
+        return next(self.colors_iter)
+
+    def visualize_tags_from_json(
+        self, json_annotation: Dict[str, Any], show_only_entities: List[str] = None
+    ):
+        """
+        Parameters
+        ----------
+        json_annotation: str
+        You can send a json that has the following format
+        {'text': str,
+        'tags': [{'start':int, 'end':str, 'tag': str}]
+        }
+        show_only_entities: List[str]
+
+        Returns
+        -------
+        None
+        """
+        text = json_annotation.get("text", None)
+        tags = json_annotation.get("tags", None)
+
+        if not text or not tags:
+            raise ValueError(
+                "The json string should have the format "
+                "{text:str, tags: [{'start': int, 'end': int, 'tag': str}]}"
+            )
+
+        all_tags = list(set(annotation["tag"] for annotation in tags))
+        len_tags = len(all_tags)
+        tag_colors = {
+            all_tags[idx]: f"{{c.on_{self._get_next_color()}}}"
+            for idx in range(len_tags)
+        }
+
+        valid_tags = {}
+        for tag in tags:
+            valid_tags[(tag["start"], tag["end"])] = tag["tag"]
+
+        # assuming that the tags are given in order
+        start_ends = []
+        for tag in tags:
+            start = tag["start"]
+            end = tag["end"]
+            start_ends.extend([start, end])
+
+        if 0 not in start_ends:
+            start_ends.insert(0, 0)
+        if len(text) not in start_ends:
+            start_ends.append(len(text))
+
+        formatted_strings = []
+        for start, end in pairwise(start_ends):
+            if valid_tags.get((start, end), None) is not None:
+                tag = valid_tags[(start, end)]
+                if show_only_entities:
+                    if tag not in show_only_entities:
+                        continue
+                tag_color = tag_colors[tag]
+                formatted_string = f"{tag_color} {text[start:end]} {{c.bold}}{tag} {colorful.close_bg_color}"
+                formatted_string = colorful.format(formatted_string)
+            else:
+                formatted_string = text[start:end]
+
+            formatted_strings.append(formatted_string)
+        tagged_string = " ".join(formatted_strings)
+        print(tagged_string)
+
+    def visualize_tokens(self, text: List[str], labels: List[str]):
+        """
+        Visualizes sequential tagged data where the string is represented as a set of words
+        and every word has a corresponding label. This can be extended to having different
+        tagging schemes at a later point in time
+
+        Parameters
+        ----------
+        text: List[str]
+        String to be tagged represented as a list of strings
+
+        labels: List[str]
+        The labels corresponding to each word in the string
+
+        Returns
+        -------
+        None
+        """
+        if len(text) != len(labels):
+            raise ValueError(
+                f"string and labels should of same length. String you passed has len {len(text)} and labels you passed has len {len(labels)}"
+            )
+        unique_labels = list(set(labels))
+        len_labels = len(unique_labels)
+
+        tag_colors = {
+            unique_labels[idx]: f"{{c.on_{self._get_next_color()}}}"
+            for idx in range(len_labels)
+        }
+
+        stylized_words = []
+
+        for idx, word in enumerate(text):
+            tag = labels[idx]
+            tag_color = tag_colors[tag]
+
+            formatted_string = (
+                f"{tag_color} word {{c.bold}}{tag.upper()} {colorful.close_bg_color}"
+            )
+            formatted_string = colorful.format(formatted_string)
+            stylized_words.append(formatted_string)
+
+        tagged_string = " ".join(stylized_words)
+        print(tagged_string)
