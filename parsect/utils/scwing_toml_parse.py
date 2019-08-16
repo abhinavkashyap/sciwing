b718c34d9e9fcd0458de959aa57c825de2eabe0d
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-08-16 21:00:47 +0800

    dataset instantiation using toml section

--- parsect/utils/scwing_toml_parse.py
+++ parsect/utils/scwing_toml_parse.py
@@ -11,6 +11,9 @@ class SciWingTOMLParse:
     def __init__(self, toml_filename: pathlib.Path):
         self.toml_filename = toml_filename
         self.doc = self._parse_toml_file()
+        self.all_datasets = (
+            None
+        )  # Dict {'train': Dataset, 'valid': Dataset, 'test': Dataset}
         self._parse()
 
     def _parse_toml_file(self):
@@ -22,36 +25,42 @@ class SciWingTOMLParse:
             print(f"File {self.toml_filename} is not found")
 
     def _parse(self):
-        dataset = self.doc.get("dataset")
+        dataset = self.doc.get(f"dataset")
         if dataset is None:
             raise TOMLConfigurationError(
-                f"{self.toml_filename} does not have a dataset section. Please "
+                f"{self.toml_filename} does not have a datasets section. Please "
                 f"Provide a dataset section in your toml file"
             )
-        self.parse_dataset_section()
+        self.all_datasets = self.parse_dataset_section()
 
     def parse_dataset_section(self):
-        dataset = self.doc.get("dataset")
+        dataset_section = self.doc.get("dataset")
+        all_datasets = {}
 
-        dataset_name = dataset.get("name")
-        dataset_classname = dataset.get("class")
+        dataset_name = dataset_section.get("name")
+        dataset_classname = dataset_section.get("class")
         if dataset_name is None or dataset_classname is None:
             raise TOMLConfigurationError(
                 f"Dataset section needs to have a name and class section"
             )
-        args = dataset.get("args")
-        try:
-            dataset_cls = create_class(
-                classname=dataset_classname,
-                module_name=ClassNursery.class_nursery[dataset_classname],
-            )
-            dataset = dataset_cls(**args)
-        except ModuleNotFoundError:
-            print(
-                f"Module {ClassNursery.class_nursery[dataset_classname]} is not found"
-            )
-        except AttributeError:
-            print(f"Class {dataset_classname} is not found ")
+        args = dataset_section.get("args")
+        for dataset_type in ["train", "valid", "test"]:
+            try:
+                dataset_cls = create_class(
+                    classname=dataset_classname,
+                    module_name=ClassNursery.class_nursery[dataset_classname],
+                )
+                args["dataset_type"] = dataset_type
+                args["filename"] = dataset_section[f"{dataset_type}_filename"]
+                dataset = dataset_cls(**args)
+                all_datasets[dataset_type] = dataset
+            except ModuleNotFoundError:
+                print(
+                    f"Module {ClassNursery.class_nursery[dataset_classname]} is not found"
+                )
+            except AttributeError:
+                print(f"Class {dataset_classname} is not found ")
+        return all_datasets
 
 
 if __name__ == "__main__":
---
+++ tests/utils/test_sciwing_toml_parse.py
@@ -0,0 +1,6 @@
+from parsect.utils.scwing_toml_parse import SciWingTOMLParse
+
+
+class TestSciWingTOMLParse:
+    def test_parse_toml_raises_error(self):
+        pass
