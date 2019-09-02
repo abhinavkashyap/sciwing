7952d115106a03a25ce8e1625dd17947fbbaa415
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-09-02 11:59:40 +0800

    custom white space tokenizer documentation

--- sciwing/utils/custom_spacy_tokenizers.py
+++ sciwing/utils/custom_spacy_tokenizers.py
@@ -1,11 +1,35 @@
+"""
+This module implements custom spacy tokenizers if needed
+This can be useful for custom tokenization that is required for scientific domain
+"""
 from spacy.tokens import Doc
 
 
 class CustomSpacyWhiteSpaceTokenizer(object):
     def __init__(self, vocab):
+        """ White space tokenizer tokenizes the word according to spaces.
+
+        Parameters
+        ----------
+        vocab : nlp.vocab
+            Spacy vocab object
+        """
         self.vocab = vocab
 
     def __call__(self, text):
+        """
+        Parameters
+        ----------
+        text : str
+            The text that should be tokenized
+
+        Returns
+        -------
+        spacy.tokens.Doc
+            Spacy docment with the tokenized text and information what is a space.
+
+
+        """
         words = text.split(" ")
         # All tokens 'own' a subsequent space character in this tokenizer
         spaces = [True] * len(words)
