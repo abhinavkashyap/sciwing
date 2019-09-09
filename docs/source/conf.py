9bc3488276431d45adc040576fb3b3a56425b04b
abhinavkashyap <abhinav@comp.nus.edu.sg>
2019-09-09 13:06:53 +0800

    index rst as entry point to docs and avoid travis builds on change to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,8 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+master_doc = "index"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
