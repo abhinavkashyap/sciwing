11e1577e0e176007e9268fbf50da5ccaa2501ed7
abhinavkashyap <abhinav@comp.nus.edu.sg>
2020-02-14 20:07:51 +0800

    parscit tagger is more general with different namespaces allowed for labels. This will replace the science ie tagger at a later point in time

--- sciwing/models/parscit_tagger.py
+++ sciwing/models/parscit_tagger.py
@@ -1,34 +1,52 @@
 import torch.nn as nn
-from typing import Dict, Any
+from typing import Dict, List
 from torchcrf import CRF
+import torch
 from sciwing.modules.lstm2seqencoder import Lstm2SeqEncoder
+from sciwing.data.datasets_manager import DatasetsManager
+from sciwing.data.seq_label import SeqLabel
+from sciwing.data.line import Line
+from collections import defaultdict
 
 
 class ParscitTagger(nn.Module):
     """PyTorch module for Neural Parscit"""
 
-    def __init__(self, rnn2seqencoder: Lstm2SeqEncoder, hid_dim: int, num_classes: int):
+    def __init__(
+        self,
+        rnn2seqencoder: Lstm2SeqEncoder,
+        encoding_dim: int,
+        datasets_manager: DatasetsManager,
+        device: torch.device = torch.device("cpu"),
+    ):
         """
 
         Parameters
         ----------
         rnn2seqencoder : Lstm2SeqEncoder
             Lstm2SeqEncoder that encodes a set of instances to a sequence of hidden states
-        hid_dim : int
+        encoding_dim : int
             Hidden dimension of the lstm2seq encoder
-        num_classes : int
-            The number of classes for every token
         """
         super(ParscitTagger, self).__init__()
         self.rnn2seqencoder = rnn2seqencoder
-        self.hidden_dim = hid_dim
-        self.num_classes = num_classes
-        self.crf = CRF(num_tags=num_classes, batch_first=True)
-        self.hidden2tag = nn.Linear(self.hidden_dim, self.num_classes)
+        self.encoding_dim = encoding_dim
+        self.datasets_manager = datasets_manager
+        self.label_namespaces = datasets_manager.label_namespaces
+        self.device = device
+        self.crfs = {}
+        self.linear_clfs = {}
+        for namespace in self.label_namespaces:
+            num_labels = self.datasets_manager.num_labels[namespace]
+            crf = CRF(num_tags=num_labels, batch_first=True)
+            clf = nn.Linear(self.encoding_dim, num_labels)
+            self.crfs[namespace] = crf
+            self.linear_clfs[namespace] = clf
 
     def forward(
         self,
-        iter_dict: Dict[str, Any],
+        lines: List[Line],
+        labels: List[SeqLabel],
         is_training: bool,
         is_validation: bool,
         is_test: bool,
@@ -36,8 +54,10 @@ class ParscitTagger(nn.Module):
         """
         Parameters
         ----------
-        iter_dict : Dict[str, Any]
-            ``iter_dict`` from any dataset that will be passed on to the encoder
+        lines : List[lines]
+            A list of lines
+        labels: List[SeqLabel]
+            A list of sequence labels
         is_training : bool
             running forward on training dataset?
         is_validation : bool
@@ -60,23 +80,50 @@ class ParscitTagger(nn.Module):
                 if this is the test dataset
         """
 
-        # batch size, time steps, hidden_dim
-        encoding = self.rnn2seqencoder(iter_dict=iter_dict)
+        # batch size, max_num_word_tokens, hidden_dim
+        encoding = self.rnn2seqencoder(lines=lines)
+        max_time_steps = encoding.size(1)
 
         # batch size, time steps, num_classes
-        logits = self.hidden2tag(encoding)
-
-        predicted_tags = self.crf.decode(logits)
-
-        output_dict = {"logits": logits, "predicted_tags": predicted_tags}
+        output_dict = {}
+        for namespace in self.label_namespaces:
+            namespace_logits = self.linear_clfs[namespace](encoding)
+            output_dict[f"logits_{namespace}"] = namespace_logits
+            output_dict[f"predicted_tags_{namespace}"] = self.crfs[namespace].decode(
+                namespace_logits
+            )
 
         if is_training or is_validation:
-            labels = iter_dict["label"]
-            assert labels.ndimension() == 2, self.msg_printer(
-                f"For Parscit tagger, labels should have 2 dimensions. "
-                f"You passed labels that have {labels.ndimension()}"
-            )
-            loss = -self.crf(logits, labels)
+            labels_indices = defaultdict(list)
+            for label in labels:
+                for namespace in self.label_namespaces:
+                    numericalizer = self.datasets_manager.namespace_to_numericalizer[
+                        namespace
+                    ]
+                    label_ = label.tokens[namespace]
+                    label_ = [tok.text for tok in label_]
+                    label_instances = numericalizer.numericalize_instance(
+                        instance=label_
+                    )
+                    label_instances = numericalizer.pad_instance(
+                        numericalized_text=label_instances,
+                        max_length=max_time_steps,
+                        add_start_end_token=False,
+                    )
+                    label_instances = torch.LongTensor(label_instances)
+                    label_instances = label_instances.to(self.device)
+                    labels_indices[namespace].append(label_instances)
+
+            losses = []
+            for namespace in self.label_namespaces:
+                labels_tensor = labels_indices[namespace]
+                labels_tensor = torch.stack(labels_tensor)
+                loss_ = -self.crfs[namespace](
+                    output_dict[f"logits_{namespace}"], labels_tensor
+                )
+                losses.append(loss_)
+
+            loss = sum(losses)
             output_dict["loss"] = loss
 
         return output_dict
--- tests/models/test_parscit_tagger.py
+++ tests/models/test_parscit_tagger.py
@@ -1,65 +1,58 @@
 import pytest
 from sciwing.models.parscit_tagger import ParscitTagger
 from sciwing.modules.lstm2seqencoder import Lstm2SeqEncoder
-from sciwing.modules.embedders.vanilla_embedder import WordEmbedder
-from sciwing.modules.charlstm_encoder import CharLSTMEncoder
+from sciwing.modules.embedders.word_embedder import WordEmbedder
+from sciwing.modules.embedders.char_embedder import CharEmbedder
 from sciwing.modules.embedders.concat_embedders import ConcatEmbedders
-import itertools
-import torch.nn as nn
-import torch
-import numpy as np
-
-lstm2encoder_options = itertools.product(
-    [True, False], ["sum", "concat"], [True, False]
+from sciwing.datasets.seq_labeling.seq_labelling_dataset import (
+    SeqLabellingDatasetManager,
 )
-lstm2encoder_options = list(lstm2encoder_options)
 
 
-@pytest.fixture(params=lstm2encoder_options)
-def setup_parscit_tagger(request):
+@pytest.fixture(scope="session")
+def seq_dataset_manager(tmpdir_factory):
+    train_file = tmpdir_factory.mktemp("train_data").join("train.txt")
+    train_file.write(
+        "word11_train###label1 word21_train###label2\nword12_train###label1 word22_train###label2 word32_train###label3"
+    )
+
+    dev_file = tmpdir_factory.mktemp("dev_data").join("dev.txt")
+    dev_file.write(
+        "word11_dev###label1 word21_dev###label2\nword12_dev###label1 word22_dev###label2 word32_dev###label3"
+    )
+
+    test_file = tmpdir_factory.mktemp("test_data").join("test.txt")
+    test_file.write(
+        "word11_test###label1 word21_test###label2\nword12_test###label1 word22_test###label2 word32_test###label3"
+    )
+
+    data_manager = SeqLabellingDatasetManager(
+        train_filename=str(train_file),
+        dev_filename=str(dev_file),
+        test_filename=str(test_file),
+    )
+
+    return data_manager
+
+
+@pytest.fixture()
+def setup_parscit_tagger(seq_dataset_manager):
     EMBEDDING_DIM = 100
-    CHARACTER_EMBEDDING_DIM = 25
-    VOCAB_SIZE = 1000
-    BATCH_SIZE = 2
-    HIDDEN_DIM = 1024
-    CHARACTER_ENCODER_HIDDEN_DIM = 100
-    NUM_TIME_STEPS = 10
-    MAX_CHAR_LENGTH = 25
-    CHAR_VOCAB_SIZE = 100
-    BIDIRECTIONAL = request.param[0]
-    COMBINE_STRATEGY = request.param[1]
-    HAVE_CHARACTER_ENCODER = request.param[2]
-    NUM_CLASSES = 5
-    EMBEDDING = nn.Embedding.from_pretrained(torch.zeros([VOCAB_SIZE, EMBEDDING_DIM]))
-    CHARACTER_EMBEDDING = nn.Embedding.from_pretrained(
-        torch.zeros([CHAR_VOCAB_SIZE, CHARACTER_EMBEDDING_DIM])
-    )
-    tokens = np.random.randint(0, VOCAB_SIZE - 1, size=(BATCH_SIZE, NUM_TIME_STEPS))
-    labels = np.random.randint(0, NUM_CLASSES - 1, size=(BATCH_SIZE, NUM_TIME_STEPS))
-    char_tokens = np.random.randint(
-        0, CHAR_VOCAB_SIZE - 1, size=(BATCH_SIZE, NUM_TIME_STEPS, MAX_CHAR_LENGTH)
-    )
-    tokens = torch.LongTensor(tokens)
-    labels = torch.LongTensor(labels)
-    char_tokens = torch.LongTensor(char_tokens)
+    HIDDEN_DIM = 100
+    BIDIRECTIONAL = True
+    COMBINE_STRATEGY = "concat"
+    dataset_manager = seq_dataset_manager
 
-    embedder = WordEmbedder(embedding=EMBEDDING, embedding_dim=EMBEDDING_DIM)
-    if HAVE_CHARACTER_ENCODER:
-        char_embedder = WordEmbedder(
-            embedding=CHARACTER_EMBEDDING, embedding_dim=CHARACTER_EMBEDDING_DIM
-        )
-        char_encoder = CharLSTMEncoder(
-            char_embedder=char_embedder,
-            char_emb_dim=CHARACTER_EMBEDDING_DIM,
-            hidden_dim=CHARACTER_ENCODER_HIDDEN_DIM,
-            bidirectional=True,
-            combine_strategy="concat",
-        )
-        embedder = ConcatEmbedders([embedder, char_encoder])
-        EMBEDDING_DIM = EMBEDDING_DIM + (2 * CHARACTER_ENCODER_HIDDEN_DIM)
+    embedder = WordEmbedder(embedding_type="glove_6B_50")
+
+    char_embedder = CharEmbedder(
+        char_embedding_dimension=10,
+        hidden_dimension=20,
+        datasets_manager=dataset_manager,
+    )
+    embedder = ConcatEmbedders([embedder, char_embedder])
 
     encoder = Lstm2SeqEncoder(
-        emb_dim=EMBEDDING_DIM,
         embedder=embedder,
         dropout_value=0.0,
         hidden_dim=HIDDEN_DIM,
@@ -70,50 +63,56 @@ def setup_parscit_tagger(request):
 
     tagger = ParscitTagger(
         rnn2seqencoder=encoder,
-        hid_dim=2 * HIDDEN_DIM
+        encoding_dim=2 * HIDDEN_DIM
         if BIDIRECTIONAL and COMBINE_STRATEGY == "concat"
         else HIDDEN_DIM,
-        num_classes=NUM_CLASSES,
+        datasets_manager=dataset_manager,
     )
 
     return (
         tagger,
+        dataset_manager,
         {
             "EMBEDDING_DIM": EMBEDDING_DIM,
-            "VOCAB_SIZE": VOCAB_SIZE,
-            "BATCH_SIZE": BATCH_SIZE,
             "HIDDEN_DIM": 2 * HIDDEN_DIM
             if BIDIRECTIONAL and COMBINE_STRATEGY == "concat"
             else HIDDEN_DIM,
             "COMBINE_STRATEGY": COMBINE_STRATEGY,
             "BIDIRECTIONAL": BIDIRECTIONAL,
-            "tokens": tokens,
-            "labels": labels,
             "EXPECTED_HIDDEN_DIM": 2 * HIDDEN_DIM
             if COMBINE_STRATEGY == "concat" and BIDIRECTIONAL
             else HIDDEN_DIM,
-            "TIME_STEPS": NUM_TIME_STEPS,
-            "NUM_CLASSES": NUM_CLASSES,
-            "HAVE_CHAR_ENCODER": HAVE_CHARACTER_ENCODER,
-            "char_tokens": char_tokens,
         },
     )
 
 
 class TestParscitTagger:
-    def test_parscit_tagger_dimensions(self, setup_parscit_tagger):
-        tagger, options = setup_parscit_tagger
-        tokens = options["tokens"]
-        labels = options["labels"]
-        char_tokens = options["char_tokens"]
-        BATCH_SIZE = options["BATCH_SIZE"]
-        TIME_STEPS = options["TIME_STEPS"]
-        NUM_CLASSES = options["NUM_CLASSES"]
-
-        iter_dict = {"tokens": tokens, "label": labels, "char_tokens": char_tokens}
+    def test_parscit_tagger_namespaces(self, setup_parscit_tagger, seq_dataset_manager):
+        tagger, dataset_manager, options = setup_parscit_tagger
+        dataset_manager = seq_dataset_manager
+        lines, labels = dataset_manager.train_dataset.get_lines_labels()
 
         output_dict = tagger(
-            iter_dict=iter_dict, is_training=True, is_validation=False, is_test=False
+            lines=lines,
+            labels=labels,
+            is_training=True,
+            is_validation=False,
+            is_test=False,
         )
 
-        assert output_dict["logits"].size() == (BATCH_SIZE, TIME_STEPS, NUM_CLASSES)
+        assert "logits_seq_label" in output_dict.keys()
+        assert "predicted_tags_seq_label" in output_dict.keys()
+        assert "loss" in output_dict.keys()
+
+    def test_parscit_tagger_dimensions(self, setup_parscit_tagger, seq_dataset_manager):
+        tagger, dataset_manager, options = setup_parscit_tagger
+        dataset_manager = seq_dataset_manager
+        lines, labels = dataset_manager.train_dataset.get_lines_labels()
+        output_dict = tagger(
+            lines=lines,
+            labels=labels,
+            is_training=True,
+            is_validation=False,
+            is_test=False,
+        )
+        assert output_dict["logits_seq_label"].size() == (2, 3, 7)
