sometimes the labels to be masked can appear in the predicted indices. Even they have to be ignored for calculation of metrices. This was happening with parscit and the bug has now been fixed

sciwing/metrics/classification_metrics_utils.py
@@ -177,7 +177,8 @@ class ClassificationMetricsUtils:
     def get_confusion_matrix_and_labels(
         predicted_tag_indices: List[List[int]],
         true_tag_indices: List[List[int]],
-        masked_label_indices: List[List[int]],
+        true_masked_label_indices: List[List[int]],
+        pred_labels_mask: List[List[int]] = None,
     ) -> (np.array, List[int]):
         """ Gets the confusion matrix and the list of classes for which the confusion matrix
         is generated
@@ -189,25 +190,39 @@ class ClassificationMetricsUtils:
             Predicted tag indices for a batch
         true_tag_indices : List[List[int]]
             True tag indices for a batch
-        masked_label_indices : List[List[int]]
+        true_masked_label_indices : List[List[int]]
             Every integer is either a 0 or 1, where 1 will indicate that the
             label in `true_tag_indices` will be ignored
         """
         # get the masked label indices
-        masked_label_indices = torch.ByteTensor(masked_label_indices).cpu()
-        masked_label_indices = torch.masked_select(
-            torch.tensor(true_tag_indices, dtype=torch.long), masked_label_indices
-        )
-        masked_label_indices = masked_label_indices.tolist()
+        true_masked_label_indices = torch.ByteTensor(true_masked_label_indices).cpu()
 
+        # select the elements in true tag indices where mask is 1
+        # these classes will not be considered for calculating the metrics
+        true_masked_label_indices = torch.masked_select(
+            torch.tensor(true_tag_indices, dtype=torch.long), true_masked_label_indices
+        )
+        true_masked_label_indices = list(set(true_masked_label_indices.tolist()))
+        masked_classes = true_masked_label_indices
+
+        # do the same for pred labels
+        if pred_labels_mask is not None:
+            pred_mask_label_indices = torch.ByteTensor(pred_labels_mask).cpu()
+            pred_mask_label_indices = torch.masked_select(
+                torch.tensor(predicted_tag_indices, dtype=torch.long),
+                pred_mask_label_indices,
+            )
+            pred_mask_label_indices = list(set(pred_mask_label_indices.tolist()))
+            masked_classes = masked_classes + pred_mask_label_indices
+
+        # get the set of unique classes
         predicted_tags_flat = list(itertools.chain.from_iterable(predicted_tag_indices))
         labels = list(itertools.chain.from_iterable(true_tag_indices))
         predicted_tags_flat = np.array(predicted_tags_flat)
         labels_numpy = np.array(labels)
-
-        # filter the classes
         classes = unique_labels(labels_numpy, predicted_tags_flat)
-        classes = filter(lambda class_: class_ not in masked_label_indices, classes)
+
+        classes = filter(lambda class_: class_ not in masked_classes, classes)
         classes = list(classes)
 
         confusion_mtrx = confusion_matrix(

sciwing/metrics/token_cls_accuracy.py
@@ -60,22 +60,25 @@ class TokenClassificationAccuracy(BaseMetric, ClassNursery):
 
         # get true labels for all namespaces
         namespace_to_true_labels = defaultdict(list)
-        namespace_to_labels_mask = defaultdict(list)
-        for label in labels:
-            for namespace in self.label_namespaces:
-                # List[List[int]]
-                predicted_tags = model_forward_dict.get(
-                    f"{self.predicted_tags_namespace_prefix}_{namespace}"
-                )
-                max_length = max(
-                    [len(tags) for tags in predicted_tags]
-                )  # max num tokens
+        namespace_to_true_labels_mask = defaultdict(list)
+        namespace_to_pred_labels_mask = defaultdict(list)
 
+        for namespace in self.label_namespaces:
+            # List[List[int]]
+            predicted_tags = model_forward_dict.get(
+                f"{self.predicted_tags_namespace_prefix}_{namespace}"
+            )
+            max_length = max([len(tags) for tags in predicted_tags])  # max num tokens
+            numericalizer = self.datasets_manager.namespace_to_numericalizer[namespace]
+            pred_tags_mask = numericalizer.get_mask_for_batch_instances(
+                instances=predicted_tags
+            )
+            namespace_to_pred_labels_mask[namespace] = pred_tags_mask
+
+            for label in labels:
                 true_labels = label.tokens[namespace]
                 true_labels = [tok.text for tok in true_labels]
-                numericalizer = self.datasets_manager.namespace_to_numericalizer[
-                    namespace
-                ]
+
                 true_labels = numericalizer.numericalize_instance(instance=true_labels)
                 true_labels = numericalizer.pad_instance(
                     numericalized_text=true_labels,
@@ -86,16 +89,16 @@ class TokenClassificationAccuracy(BaseMetric, ClassNursery):
                     instance=true_labels
                 ).tolist()
                 namespace_to_true_labels[namespace].append(true_labels)
-                namespace_to_labels_mask[namespace].append(labels_mask)
+                namespace_to_true_labels_mask[namespace].append(labels_mask)
 
         for namespace in self.label_namespaces:
             labels_ = namespace_to_true_labels[namespace]
-            labels_mask = namespace_to_labels_mask[namespace]
+            labels_mask_ = namespace_to_true_labels_mask[namespace]
+            pred_labels_mask_ = namespace_to_pred_labels_mask[namespace]
             # List[List[int]]
             predicted_tags = model_forward_dict.get(
                 f"{self.predicted_tags_namespace_prefix}_{namespace}"
             )
-            labels_mask = torch.LongTensor(labels_mask).type(torch.ByteTensor).tolist()
 
             (
                 confusion_mtrx,
@@ -103,7 +106,8 @@ class TokenClassificationAccuracy(BaseMetric, ClassNursery):
             ) = self.classification_metrics_utils.get_confusion_matrix_and_labels(
                 true_tag_indices=labels_,
                 predicted_tag_indices=predicted_tags,
-                masked_label_indices=labels_mask,
+                true_masked_label_indices=labels_mask_,
+                pred_labels_mask=pred_labels_mask_,
             )
 
             tps = np.around(np.diag(confusion_mtrx), decimals=4)
@@ -281,7 +285,7 @@ class TokenClassificationAccuracy(BaseMetric, ClassNursery):
         ) = self.classification_metrics_utils.get_confusion_matrix_and_labels(
             predicted_tag_indices=predicted_tag_indices,
             true_tag_indices=true_tag_indices,
-            masked_label_indices=labels_mask,
+            true_masked_label_indices=labels_mask,
         )
 
         classes_with_names = classes