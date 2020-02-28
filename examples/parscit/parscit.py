crf fix with one for the mask - not masking anything

examples/parscit/parscit.py
@@ -1,5 +1,5 @@
 from sciwing.modules.lstm2seqencoder import Lstm2SeqEncoder
-from sciwing.models.simple_tagger import SimpleTagger
+from sciwing.models.rnn_seq_crf_tagger import RnnSeqCrfTagger
 from sciwing.modules.embedders.trainable_word_embedder import TrainableWordEmbedder
 from sciwing.modules.embedders.concat_embedders import ConcatEmbedders
 from sciwing.datasets.seq_labeling.seq_labelling_dataset import (
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         device=torch.device(args.device),
         dropout_value=0.1,
     )
-    model = SimpleTagger(
+    model = RnnSeqCrfTagger(
         rnn2seqencoder=lstm2seqencoder,
         encoding_dim=2 * args.hidden_dim
         if args.bidirectional and args.combine_strategy == "concat"

sciwing/models/rnn_seq_crf_tagger.py
@@ -127,9 +127,6 @@ class RnnSeqCrfTagger(nn.Module, ClassNursery):
             predicted_tags = [tag for tag, _ in predicted_tags]
             output_dict[f"predicted_tags_{namespace}"] = predicted_tags
 
-            print(f"Predicted tag")
-            print(predicted_tags[0])
-
         if is_training or is_validation:
             labels_indices = defaultdict(list)
             for label in labels:
@@ -157,15 +154,15 @@ class RnnSeqCrfTagger(nn.Module, ClassNursery):
                 dtype=torch.long,
                 device=self.device,
             )
-            mask = get_mask(
-                batch_size=len(lines), max_size=max_time_steps, lengths=len_tokens
-            )
-            mask = mask.to(self.device)
 
             losses = []
             for namespace in self.label_namespaces:
                 labels_tensor = labels_indices[namespace]
                 labels_tensor = torch.stack(labels_tensor)
+                batch_size, time_steps = labels_tensor.size()
+                mask = torch.ones(
+                    size=(batch_size, time_steps), dtype=torch.long, device=self.device
+                )
                 logits_namespace = output_dict[f"logits_{namespace}"]
                 loss_ = -self.crfs[namespace](logits_namespace, labels_tensor, mask)
                 losses.append(loss_)