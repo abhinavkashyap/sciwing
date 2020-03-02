6f6f0059263c596326588be166bdc2453d506d49
abhinavkashyap <abhinav@comp.nus.edu.sg>
2020-03-02 20:21:10 +0800

    generic sect bilstm file

--- examples/genericsect_bilstm/genericsect_bilstm_infer.py
+++ examples/genericsect_bilstm/genericsect_bilstm_infer.py
@@ -9,6 +9,9 @@ from sciwing.infer.classification.classification_inference import (
 from sciwing.datasets.classification.text_classification_dataset import (
     TextClassificationDatasetManager,
 )
+from sciwing.infer.interface_client_base import BaseInterfaceClient
+from sciwing.cli.sciwing_interact import SciWINGInteract
+from typing import Dict, Any
 import pathlib
 
 PATHS = constants.PATHS
@@ -16,53 +19,65 @@ DATA_DIR = PATHS["DATA_DIR"]
 DATA_DIR = pathlib.Path(DATA_DIR)
 
 
-def build_genericsect_bilstm_model(dirname: str):
+class BuildGenericSectBiLSTMInfer(BaseInterfaceClient):
+    def __init__(self, hparams: Dict[str, Any]):
+        self.hparams = hparams
+        self.data_manager = self.build_dataset()
+        self.model = self.build_model()
 
-    exp_dirpath = pathlib.Path(dirname)
+    def build_model(self):
+        embedder = WordEmbedder(embedding_type=self.hparams.get("embedding_type"))
 
-    EMBEDDING_TYPE = "glove_6B_50"
-    HIDDEN_DIM = 512
-    COMBINE_STRATEGY = "concat"
-    BIDIRECTIONAL = True
+        encoder = LSTM2VecEncoder(
+            embedder=embedder,
+            hidden_dim=self.hparams.get("hidden_dim"),
+            combine_strategy=self.hparams.get("combine_strategy"),
+            bidirectional=self.hparams.get("bidirectional"),
+        )
 
-    train_filename = DATA_DIR.joinpath("genericSect.train")
-    dev_filename = DATA_DIR.joinpath("genericSect.dev")
-    test_filename = DATA_DIR.joinpath("genericSect.test")
+        model = SimpleClassifier(
+            encoder=encoder,
+            encoding_dim=2 * self.hparams.get("hidden_dim"),
+            num_classes=self.hparams.get("num_classes"),
+            classification_layer_bias=True,
+            datasets_manager=self.data_manager,
+        )
 
-    data_manager = TextClassificationDatasetManager(
-        train_filename=train_filename,
-        dev_filename=dev_filename,
-        test_filename=test_filename,
-    )
+        return model
 
-    embedder = WordEmbedder(embedding_type=EMBEDDING_TYPE)
+    def build_dataset(self):
+        train_filename = DATA_DIR.joinpath("genericSect.train")
+        dev_filename = DATA_DIR.joinpath("genericSect.dev")
+        test_filename = DATA_DIR.joinpath("genericSect.test")
 
-    encoder = LSTM2VecEncoder(
-        embedder=embedder,
-        hidden_dim=HIDDEN_DIM,
-        combine_strategy=COMBINE_STRATEGY,
-        bidirectional=BIDIRECTIONAL,
-    )
+        data_manager = TextClassificationDatasetManager(
+            train_filename=train_filename,
+            dev_filename=dev_filename,
+            test_filename=test_filename,
+        )
+        return data_manager
 
-    model = SimpleClassifier(
-        encoder=encoder,
-        encoding_dim=2 * HIDDEN_DIM,
-        num_classes=12,
-        classification_layer_bias=True,
-        datasets_manager=data_manager,
-    )
+    def build_infer(self):
+        inference = ClassificationInference(
+            model=self.model,
+            model_filepath=self.hparams.get("model_filepath"),
+            datasets_manager=self.data_manager,
+        )
 
-    inference = ClassificationInference(
-        model=model,
-        model_filepath=str(exp_dirpath.joinpath("checkpoints", "best_model.pt")),
-        datasets_manager=data_manager,
-    )
-
-    return inference
+        return inference
 
 
 if __name__ == "__main__":
     dirname = pathlib.Path(".", "output")
-    infer = build_genericsect_bilstm_model(str(dirname))
-    infer.run_inference()
-    infer.report_metrics()
+    model_filepath = dirname.joinpath("checkpoints", "best_model.pt")
+    hparams = {
+        "embedding_type": "glove_6B_50",
+        "hidden_dim": 512,
+        "bidirectional": True,
+        "combine_strategy": "concat",
+        "num_classes": 12,
+        "model_filepath": model_filepath,
+    }
+    infer = BuildGenericSectBiLSTMInfer(hparams=hparams)
+    cli = SciWINGInteract(infer_client=infer)
+    cli.interact()
