e87e42cd2690e1c6c06135218a5729062363dae5
abhinavkashyap <abhinav@comp.nus.edu.sg>
2020-07-09 15:10:50 +0800

    sciwing interact is a command that helps you interact with the pretrained model for diagnosis

--- sciwing/commands/sciwing_group.py
+++ sciwing/commands/sciwing_group.py
@@ -4,6 +4,7 @@ from sciwing.commands.run import run
 from sciwing.commands.test import test
 from sciwing.commands.develop import develop
 from sciwing.commands.download import download
+from sciwing.commands.interact import interact
 
 
 @click.group(name="sciwing")
@@ -19,6 +20,7 @@ def main():
     sciwing_group.add_command(test)
     sciwing_group.add_command(develop)
     sciwing_group.add_command(download)
+    sciwing_group.add_command(interact)
     sciwing_group()
 
 
