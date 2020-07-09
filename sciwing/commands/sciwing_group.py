7d96da44685a4ec9665b7519c8ad9af6a7790acd
abhinavkashyap <abhinav@comp.nus.edu.sg>
2020-07-09 20:31:34 +0800

    removed sciwing command new

--- sciwing/commands/sciwing_group.py
+++ sciwing/commands/sciwing_group.py
@@ -1,5 +1,4 @@
 import click
-from sciwing.commands.new import new
 from sciwing.commands.run import run
 from sciwing.commands.test import test
 from sciwing.commands.develop import develop
@@ -15,7 +14,6 @@ def sciwing_group():
 
 
 def main():
-    sciwing_group.add_command(new)
     sciwing_group.add_command(run)
     sciwing_group.add_command(test)
     sciwing_group.add_command(develop)
